cc8c3d2c76b8d3525fb6767206210d3c3731ae35
sinamonY <75080364+sinamonY@users.noreply.github.com>
2020-12-24 11:05:09 +0900

    アプリプロパ、htmlファイル、pom作成、コントローラの名前変更

--- MyBootAppEach-2/src/main/java/com/example/demo/HeloYuriko.java
+++ MyBootAppEach-2/src/main/java/com/example/demo/HeloYuriko.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Each {
+public class HeloYuriko {
 
 	@RequestMapping(value="/")
 	public String index() {
-		return "Helo yuriko";
+		return "HeloYuriko";
 	}
 }
