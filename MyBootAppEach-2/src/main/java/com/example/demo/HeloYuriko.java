80a46130d8a74fba621afff307c67b75af058921
sinamonY <75080364+sinamonY@users.noreply.github.com>
2021-01-13 01:38:04 +0900

    タイムリーフの使い方など学んだ

--- MyBootAppEach-2/src/main/java/com/example/demo/HeloYuriko.java
+++ MyBootAppEach-2/src/main/java/com/example/demo/HeloYuriko.java
@@ -1,62 +1,95 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-//@RestController
+//@Controller
 //public class HeloYuriko {
 //
-//	@RequestMapping(value="/")
-//	public String index() {
-//		return "HeloYuriko";
+//	@RequestMapping("/{num}")
+//	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+//		int res  = 0;
+//		for(int i=1; i<=num; i++)
+//			res += i;
+//		mav.addObject("msg", "total:" + res);
+//      mav.setViewName("HeloYuriko");
+//		return mav;
 //	}
 //}
-@Controller //htmlを参照してページをつくる
+
+@Controller
 public class HeloYuriko{
 
-	@RequestMapping(value="/",method=RequestMethod.GET) //ゲットメソッドなのでクレデンシャル情報も載っちゃう？
-	public ModelAndView index(ModelAndView mav) { //モデル＆ビュークラス型のmav変数をインデックスmethodの引数に。
-		mav.setViewName("index"); //"インデックス"ってなに？indexなんて表示しないよね。setViewName調べる！→index.htmlを参照する。
-		mav.addObject("msg","フォームを送信してください");
+//	@RequestMapping(value="/",method=RequestMethod.GET) //先にPOSTを書いてもGETが先に処理される。ゲットメソッドなのでクレデンシャル情報も載っちゃう？
+//	public ModelAndView index(ModelAndView mav) { //モデル＆ビュークラス型のmav変数をインデックスmethodの引数に。
+//		mav.addObject("msg","名前を入力して送信してください");
+//		mav.setViewName("HeloYuriko"); //""内はhtmlファイルの名称
+//		return mav;
+//	}
+//
+//	@RequestMapping(value="/",method=RequestMethod.POST)
+//	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mv) {
+//		mv.addObject("msg","こんにちは、" + str + "さん");
+//		mv.addObject("value",str);
+//		mv.setViewName("HeloYuriko");
+//		return mv;
+//	}
+//
+//	@RequestMapping(value="/",method=RequestMethod.POST) //引数をあたえてJavaを動かす？
+//	public ModelAndView send( //sendメソッド調べてみる。
+//			@RequestParam(value="check1",required=false)boolean check1, //required=falseを書いておけば選択なくても例外おこらない☆
+//			@RequestParam(value="radio1",required=false)String radio1,
+//			@RequestParam(value="select1",required=false)String select1,
+//			@RequestParam(value="select2",required=false)String[] select2,
+//			ModelAndView av) {
+//
+//	String res ="";
+//	try {
+//		res = "check:" + check1 + " radio:" + radio1 + " select:" + select1 + " \nselect2:";
+//		}
+//	catch (NullPointerException e) {}
+//
+//	try {
+//		res += select2[0];
+//		for(int i=1; i<select2.length; i++)
+//			res += ","  + select2[i];
+//		}
+//	catch(NullPointerException e) {
+//		res += "null";
+//	}
+//	av.addObject("tyantyan",res);
+//	av.setViewName("HeloYuriko");
+//	return av;
+//	}
+
+//@RequestMapping("/") //WEBページのフォワードとリダイレクトの挙動確認プログラム（以下３つのリクマップ）
+//public ModelAndView index(ModelAndView mav) {
+//	mav.setViewName("HeloYuriko");
+//	return mav;
+//}
+//
+//@RequestMapping("/other")
+//public String other() {
+//	return "redirect:/";
+//}
+//
+//@RequestMapping("/home")
+//public String home() {
+//	return "forward:/";
+
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("HeloYuriko");
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"taro","taro@yamada","000-0000-0000"});
+		data.add(new String[] {"taro","hanako@yamada","000-0000-0000"});
+		data.add(new String[] {"taro","satie@yamada","000-0000-0000"});
+		mav.addObject("data",data);
 		return mav;
 	}
 
-	@RequestMapping(value="/",method=RequestMethod.POST)
-	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
-		mav.addObject("msg","こんにちは、" + str + "さん");
-		mav.addObject("value",str);
-		mav.setViewName("index");
-		return mav;
-	}
 
-
-	@RequestMapping(value="/",method=RequestMethod.POST) //引数をあたえてJavaを動かす？
-	public ModelAndView send( //sendメソッド調べてみる。
-			@RequestParam(value="check1",required=false)boolean check1, //required=falseを書いておけば選択なくても例外おこらない☆
-			@RequestParam(value="radio1",required=false)String radio1,
-			@RequestParam(value="select1",required=false)String select1,
-			@RequestParam(value="select21",required=false)String[] select2,
-			ModelAndView mav) {
-
-	String res ="";
-	try {
-		res = "check:" + check1 + "radio:" + radio1 + "select:" + select1 + "\nselect2:";
-		}
-	catch (NullPointerException e) {}
-
-	try {
-		res = res + select2[0];
-		for(int i=1; i<select2.length; i++)
-			res = res + ","  + select2[i];
-		}
-	catch(NullPointerException e) {
-		res = res + "null";
-	}
-	mav.addObject("msg",res);
-	mav.setViewName("index");
-	return mav;
-	}
 }
